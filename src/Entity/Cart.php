61ca16889d08d55c3d54de476dc7f41cfe9d89d0
Julien Mfochive <julien@liant.dev>
2023-04-21 09:05:54 +0200

    Premier commit : Ajout de mes fichiers de projet.

---
+++ src/Entity/Cart.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CartRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CartRepository::class)]
+class Cart
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'carts')]
+    private ?user $CartUser = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $Date = null;
+
+    #[ORM\Column]
+    private ?bool $State = null;
+
+    #[ORM\OneToMany(mappedBy: 'Cart', targetEntity: CartContent::class)]
+    private Collection $cartContents;
+
+    public function __construct()
+    {
+        $this->cartContents = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCartUser(): ?user
+    {
+        return $this->CartUser;
+    }
+
+    public function setCartUser(?user $CartUser): self
+    {
+        $this->CartUser = $CartUser;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function isState(): ?bool
+    {
+        return $this->State;
+    }
+
+    public function setState(bool $State): self
+    {
+        $this->State = $State;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CartContent>
+     */
+    public function getCartContents(): Collection
+    {
+        return $this->cartContents;
+    }
+
+    public function addCartContent(CartContent $cartContent): self
+    {
+        if (!$this->cartContents->contains($cartContent)) {
+            $this->cartContents->add($cartContent);
+            $cartContent->setCart($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCartContent(CartContent $cartContent): self
+    {
+        if ($this->cartContents->removeElement($cartContent)) {
+            // set the owning side to null (unless already changed)
+            if ($cartContent->getCart() === $this) {
+                $cartContent->setCart(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    #[ORM\Column]
+    private ?int $Price = null;
+
+    #[ORM\Column]
+    private ?int $Stock = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\ManyToOne(inversedBy: 'product')]
+    private ?CartContent $cartContent = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(int $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->Stock;
+    }
+
+    public function setStock(int $Stock): self
+    {
+        $this->Stock = $Stock;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCartContent(): ?CartContent
+    {
+        return $this->cartContent;
+    }
+
+    public function setCartContent(?CartContent $cartContent): self
+    {
+        $this->cartContent = $cartContent;
+
+        return $this;
+    }
+}
